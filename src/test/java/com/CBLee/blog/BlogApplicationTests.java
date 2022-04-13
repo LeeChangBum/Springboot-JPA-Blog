c1aeccec1918ea000291e86794c65c0b57130f86
lcb4843 <lcb4843@naver.com>
2022-04-13 13:12:34 +0900

    환경세팅 완료

---
+++ src/test/java/com/CBLee/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CBLee.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
